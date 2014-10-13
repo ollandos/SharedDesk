55bfccc7cf25f78aa253b0b8199207f254eab450
kanekin0812 <tatsuya@sense-os.nl>
2014-10-13 16:36:57 +0100

    organized ConsoleObserver

--- FileTransfer v4/StrategyPatternExample/StrategyPatternExample/Observers/ConsoleObserver.cs
+++ FileTransfer v4/StrategyPatternExample/StrategyPatternExample/Observers/ConsoleObserver.cs
@@ -12,18 +12,7 @@ namespace StrategyPatternExample.Observers
 
         public void notify(string filePath, System.Net.IPEndPoint endPoint, bool sending)
         {
-            if (sending)
-            {
-                Console.WriteLine("*** SENDING FILE ***");
-                Console.WriteLine(String.Format("TO IP: {0} : {1}", endPoint.Address.ToString(), endPoint.Port));
-            }
-            else
-            {
-                Console.WriteLine("*** RECEIVING FILE ***");
-                Console.WriteLine(String.Format("FROM IP: {0} : {1}", endPoint.Address.ToString(), endPoint.Port));
-            }
-
-            Console.WriteLine(String.Format("FILE: {0}", filePath));
+            notify(filePath, endPoint, sending, false);
         }
 
         public void notify(string filePath, System.Net.IPEndPoint endPoint, bool sending, bool transferComplete)
