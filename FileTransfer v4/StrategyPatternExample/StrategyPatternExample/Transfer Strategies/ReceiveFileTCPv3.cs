62d237079c13e61247a76aa41ffe37fb64addbdf
Jan Sviland <jan@sviland.org>
2014-10-20 19:02:13 +0200

    fixed a small problem with the file transfer

--- FileTransfer v4/StrategyPatternExample/StrategyPatternExample/Transfer Strategies/ReceiveFileTCPv3.cs
+++ FileTransfer v4/StrategyPatternExample/StrategyPatternExample/Transfer Strategies/ReceiveFileTCPv3.cs
@@ -48,6 +48,7 @@ namespace StrategyPatternExample.Transfer_Strategies
         {
             this.receivePath = filePath;
             this.remotePoint = remotePoint;
+
             // start thread
             this.t1 = new Thread(new ThreadStart(StartListening));
             this.t1.Start();
@@ -74,12 +75,12 @@ namespace StrategyPatternExample.Transfer_Strategies
         public void StartListening()
         {
 
-            //Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            //listener.ReceiveBufferSize = StateObject.BufferSize;
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.ReceiveBufferSize = StateObject.BufferSize;
             //listener.DontFragment = true;
 
-            TcpListener listener2 = new TcpListener(remotePoint);
-            listener2.AllowNatTraversal(true);
+            //TcpListener listener2 = new TcpListener(remotePoint);
+            //listener2.AllowNatTraversal(true);
 
             // Don't allow another socket to bind to this port.
             // Should perhaps consider this one
@@ -88,14 +89,14 @@ namespace StrategyPatternExample.Transfer_Strategies
             try
             {
 
-                //listener.Bind(this.remotePoint);
-                //listener.Listen(100);
+                listener.Bind(this.remotePoint);
+                listener.Listen(100);
 
                 while (true)
                 {
                     allDone.Reset();
-                    //listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
-                    listener2.BeginAcceptSocket(new AsyncCallback(AcceptCallback), listener2);
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+                    //listener2.BeginAcceptSocket(new AsyncCallback(AcceptCallback), listener2);
                     allDone.WaitOne();
                 }
             }
