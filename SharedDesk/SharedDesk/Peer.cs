fcc48677446282c0bae15d6f5ae6a59676a17c01
John Triantafillou <john@qlvr.nl>
2014-11-05 19:50:44 +0100

    doing stuff

---
+++ SharedDesk/SharedDesk/Peer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedDesk
+{
+    class Peer
+    {
+        List<PeerInfo> routingTable;
+        PeerInfo bootPeer;
+        int GUID;
+
+        public Peer(int guid) {
+            routingTable = new List<PeerInfo>();
+            bootPeer = new PeerInfo(0, "192.168.1.x", 6666);
+            routingTable.Add(bootPeer);
+            GUID = guid;
+        }
+
+    }
+}
