016e42e6fe39be1d1951b2412778bccdd85205e3
John Triantafillou <john@qlvr.nl>
2014-11-05 21:07:00 +0100

    wtf

--- SharedDesk/SharedDesk/Peer.cs
+++ SharedDesk/SharedDesk/Peer.cs
@@ -22,67 +22,46 @@ namespace SharedDesk
         public void makeTable(Dictionary<int, Peer> net)
         {
             RoutingTable newRoutingTable = new RoutingTable();
-            List<int> targetGUIDs = getTargetGUIDs();
+            List<int> targetGUIDs = newRoutingTable.getTargetGUIDs(GUID);
             foreach (int guid in targetGUIDs)
             {
                 //Find closest peer in own table
-                PeerInfo closest = routingTable.findClosestTest(guid);
+                PeerInfo closest = routingTable.findClosest(-1, guid);
                 //Sets target
                 int target = guid;
-                
                 PeerInfo prevClosest = null;
                 while (closest.getGUID() != target && closest != prevClosest)
                 {
                     prevClosest = closest;
                     //closest = net[closest].askForClosestPeer(GUID, closest, target);
                 }
-                //check duplicate
-                //If the peer already exist on the table we ignore.
-                //If it does not exist add it to the table, and contact peer so that the other peer can register this peer too.
+                //If found PeerInfo does not exist add it to the table.
                 bool isDuplicated = newRoutingTable.Contains(closest);
                 if (!isDuplicated && closest.getGUID() != GUID)
                 {
                     newRoutingTable.Add(closest);
                 }
-                isDuplicated = newRoutingTable.Contains(prevClosest);
             }
-            if (newRoutingTable.Count != 0)
+            if (newRoutingTable.Count() != 0)
             {
                 routingTable = newRoutingTable;
             }
         }
 
-        public int askForClosestPeer(int guid, int Closest, int Target)
-        {
-            int closest = GUID;
-            int target = Target;
-            foreach (int p in RoutingTable)
-            {
-                if (calculateXOR(p, target) < calculateXOR(closest, target) && guid != p)
-                {
-                    closest = p;
-                }
-            }
-            return closest;
-        }
+        //public int askForClosestPeer(int senderGUID, int Target)
+        //{
+        //    int closest = GUID;
+        //    int target = Target;
+        //    foreach (int p in table)
+        //    {
+        //        if (calculateXOR(p, target) < calculateXOR(closest, target) && senderGUID != p)
+        //        {
+        //            closest = p;
+        //        }
+        //    }
+        //    return closest;
+        //}
 
         
-
-        
-
-        public List<int> getTargetGUIDs()
-        {
-            List<int> targetGUIDs = new List<int>();
-
-            double exponentCapacity = 4;
-            for (double x = 0; x < exponentCapacity; x += 1d)
-            {
-                double result = Math.Pow(2, x);
-                result = calculateXOR(this.GUID, (int)result);
-                targetGUIDs.Add((int)result);
-            }
-
-            return targetGUIDs;
-        }
     }
 }
