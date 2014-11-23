b92039011011da52a92715c1ff08168e036da61b
John Triantafillou <john@qlvr.nl>
2014-11-23 21:57:10 +0100

    cleaning up code

--- SharedDesk/SharedDesk/Kadelima/Peer.cs
+++ SharedDesk/SharedDesk/Kadelima/Peer.cs
@@ -19,13 +19,9 @@ namespace SharedDesk
         private int GUID;
         //private const int DEFAULT_LISTENING_PORT = 6666;
 
-        /// <summary>
-        /// EVENTS FOR FORM
-        /// </summary>
+        // Event to update Form
         public event handlerUpdatedTable updateTable;
         public delegate void handlerUpdatedTable();
-
-        private RoutingTable mNewRoutingTable;
 
         private List<SearchChannel> channels;
 
@@ -59,64 +55,6 @@ namespace SharedDesk
             responder.sendRequestRoutingTable();
         }
 
-        /*
-        //Refresing/Creating the routingTable
-        public void makeTable()
-        {
-            RoutingTable newRoutingTable = new RoutingTable(routingTable.getMyInfo);
-            List<int> targetGUIDs = newRoutingTable.getTargetGUIDs(GUID);
-            foreach (int guid in targetGUIDs)
-            {
-                //Find closest peer in own table
-                PeerInfo closest = routingTable.findClosest(guid);
-                //Sets target
-                int target = guid;
-                PeerInfo prevClosest = null;
-                while (closest.getGUID != target && closest != prevClosest)
-                {
-                    prevClosest = closest;
-                    //closest = net[closest].askForClosestPeer(GUID, target);
-                }
-                //If found PeerInfo does not exist add it to the table.
-                bool isDuplicated = newRoutingTable.Contains(closest);
-                if (!isDuplicated && closest.getGUID != GUID)
-                {
-                    newRoutingTable.Add(closest);
-                }
-            }
-            if (newRoutingTable.Count() != 0)
-            {
-                routingTable = newRoutingTable;
-            }
-        }
-         * */
-
-        //JOINING THE NETWORK
-        //Joining Peer will tell all peers in his table to add him.
-        public void joinToTable()
-        {
-            //foreach (PeerInfo p in routingTable.getPeers())
-            //{
-                    //p.Value.acceptPeer(PeerInfo);
-                //p.Value.acceptPeer(routingTable.get);
-
-            //}
-        }
-
-        /*
-        //Accepting newly joined Peer
-        public void acceptPeer(PeerInfo peer)
-        {
-            bool isDuplicated = routingTable.Contains(peer);
-
-            if (!isDuplicated)
-            {
-                routingTable.Add(peer);
-            }
-
-        }
-         * */
-
         //Responding with closest found Peer
         public PeerInfo askForClosestPeer(int senderGUID, int target)
         {
@@ -126,8 +64,7 @@ namespace SharedDesk
         //Searches for closest peers in the network
         private void searchTargetPeers()
         {
-            mNewRoutingTable = new RoutingTable(routingTable.MyInfo);
-            List<int> targetGUIDs = mNewRoutingTable.getTargetGUIDs(GUID);
+            List<int> targetGUIDs = routingTable.getTargetGUIDs(GUID);
             foreach (int guid in targetGUIDs)
             {
                 PeerInfo closest = routingTable.findClosest(guid);
@@ -137,7 +74,6 @@ namespace SharedDesk
             }
             routingTable.cleanTable(GUID);
         }
-
 
         public int getGUID
         {
@@ -194,7 +130,6 @@ namespace SharedDesk
                     //PeerInfo removingPeer = routingTable.get(targetGUID);
                     //notify to the peer with the targetGUID
                     //UDPResponder responder = new UDPResponder(remotePoint, DEFAULT_LISTENING_PORT);
-
                     routingTable.replace(targetGuid, newPeer);
                 }
                 else
@@ -204,10 +139,6 @@ namespace SharedDesk
                 searchTargetPeers();
                 updateTable();
             }
-            //TOOD: think about cycle of deleting non-target guids
-            // Probably we want to remove those based when we find closer GUID.
-            //Every time we find the closer peer we discard the peer which was previously occupying the key.
-            //And right before discarding, we send leave request to the peer which is about to be removed.
         }
 
 
